b51b063987b1dc8f53485c948a5e5e454f6e6a9e
unknown <>
2017-04-04 20:14:25 +0800

    add the login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,25 +1,52 @@
 <template>
-  <div>
+  <div class="login-con">
     <div class="icon">
-      <img src="../../../static/img/logo.jpg" alt="" width="30" height="30">
+      <img src="../../../static/img/logo.jpg" alt="" width="200" height="200">
     </div>
-    <div class="login-con form-group">
-      <input type="text">
-      mjj;l
-      <div class="input-group">
-      <button class="btn btn-primary" @click="login">login</button>
-      </div>
+    <div class="loginBtn-con">
+      <input type="text" v-model="loginKey">
+      <button  class="btn btn-primary login-btn m-t-lg" @click="login" onkeyup.enter="login">login</button>
     </div>
   </div>
 </template>
 
 <script>
-// import path from 'path'
+import path from 'path'
 export default {
-  actions: {
-    lgoin () {
-      // this.$router.push(path.index)
+  data () {
+    return {
+      loginKey: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$router.push({path: path.index.index, query: {key: this.loginKey}})
     }
   }
 }
 </script>
+
+<style scoped>
+.login-con{
+  background-color: #ffffff;
+}
+.icon{
+  margin-top: 20%;
+}
+.loginBtn-con{
+  margin: 0 auto;
+  margin-top: 30%;
+  width: 70%;
+}
+
+.loginBtn-con input{
+  padding-left: 10px;
+  width: 100%;
+  border: 2px solid #bdc3c7;  
+  border-radius: 4px;
+}
+
+.login-btn{
+  width: 100%;
+}
+</style>
